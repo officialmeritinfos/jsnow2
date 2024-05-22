eb668e5c143568d93c16a378da885e592e7184e9
Anon Dev <michaelerastus9@gmail.com>
2024-05-22 12:04:36 +0100

    updated

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -44,16 +44,16 @@
 <body>
 @inject('injected','App\Defaults\Custom')
 <!-- preloader -->
-{{--<div id="loading">--}}
-{{--    <div id="loading-center">--}}
-{{--        <div id="loading-center-absolute">--}}
-{{--            <div class="object" id="object_four"></div>--}}
-{{--            <div class="object" id="object_three"></div>--}}
-{{--            <div class="object" id="object_two"></div>--}}
-{{--            <div class="object" id="object_one"></div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+<div id="loading">
+    <div id="loading-center">
+        <div id="loading-center-absolute">
+            <div class="object" id="object_four"></div>
+            <div class="object" id="object_three"></div>
+            <div class="object" id="object_two"></div>
+            <div class="object" id="object_one"></div>
+        </div>
+    </div>
+</div>
 <!-- topbar starts -->
 <div class="top-bar">
     <div class="container">
